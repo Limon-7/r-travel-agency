72888e68042ea92e2824282900306ac5a4868a09
Limon <limon14203165@gmail.com>
2020-08-31 13:34:06 +0600

    Project setup

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <img
+                className="header_icon"
+                src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/102014/airbnb.png?itok=d2X_Ds1a"
+                alt=""
+            />
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/constants/Technology.js
@@ -0,0 +1,9 @@
+// Materiaul UI
+// React Hook
+// React Router
+// Firebase Hosting
+// Flexboox
+// Resposive AF
+// grow/shrink Animation
+// CSS game
+// Date picker
